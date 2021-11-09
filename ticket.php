ae26bf71f93e3ef783c1aaf89e47db64a90abc3c
rain <45768231+cyber-land@users.noreply.github.com>
2021-11-09 16:51:52 +0100

    ticket exercise
    improvement

--- ticket.php
+++ ticket.php
@@ -8,35 +8,29 @@
  */
 require 'vendor/autoload.php';
 $templates = new League\Plates\Engine('templates', 'phtml');
-$price = 1.0;
+$price = 0;
 $zone = $_POST['zone'];
 $type = $_POST['ticket_type'];
+$quantity = $_POST['quantity'];
 if (is_numeric($zone)) {
     switch ($zone) {
         case 1:
-            $price = 1.4;
-            break;
+            $price = 1.4; break;
         case 2:
-            $price = 1.8;
-            break;
+            $price = 1.8; break;
         case 3:
-            $price = 2.2;
-            break;
+            $price = 2.2; break;
     }
 }
 switch ($type) {
     case 'one-way':
-        $price;
-        break;
+        $price; break; //it doesn't change
     case 'hourly':
-        $price*=1.5;
-        break;
+        $price*=1.5; break;
     case 'day':
-        $price*=2;
-        break;
+        $price*=2; break;
 };
 $ids = [];
-$quantity = $_POST['quantity'];
 for($i = 0; $i < $quantity; $i++) {
     array_push($ids, bin2hex(random_bytes(9)));
 }
